1965d16aa8a0c196e7d0ef3dc004e86f22d11e33
namrata678 <130288000+namrata678@users.noreply.github.com>
2023-11-18 23:33:32 +0930

    Add files via upload

--- StudentManagementSystem.java
+++ StudentManagementSystem.java
@@ -1,3 +1,8 @@
+/*
+ * StudentManagementSystem.java
+ * Coder: Namrata Ingle
+ * Copyright (c) [2023] Namrata Ingle. All rights reserved.
+ */
 
 import java.util.ArrayList;
 import java.util.List;
@@ -5,34 +10,27 @@ import java.util.List;
 public class StudentManagementSystem {
     private List<Student> students;
 
-    // Constructor
     public StudentManagementSystem() {
         this.students = new ArrayList<>();
     }
 
-    // Methods
-
-    // Add a student
     public void addStudent(Student student) {
         students.add(student);
     }
 
-    // Remove a student
     public void removeStudent(Student student) {
         students.remove(student);
     }
 
-    // Search for a student by roll number
     public Student searchStudent(int rollNumber) {
         for (Student student : students) {
             if (student.getRollNumber() == rollNumber) {
                 return student;
             }
         }
-        return null; // Student not found
+        return null;
     }
 
-    // Display all students
     public void displayAllStudents() {
         for (Student student : students) {
             System.out.println(student);
